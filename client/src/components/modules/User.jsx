f665014630622ff31f8db0aa3ec6b5a3a7f9b5aa
Regan <songregan@gmail.com>
2025-01-14 17:53:50 -0500

    users jsx and css files

---
+++ client/src/components/modules/User.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+import "./SingleUser.css";
+
+/* proptypes
+ *   @param {UserObject} user
+ *   @param {boolean} active
+ */
+
+const SingleUser = (props) => {
+  return <div></div>;
+};
+
+export default SingleUser;
