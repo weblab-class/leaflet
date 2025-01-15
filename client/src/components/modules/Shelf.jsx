ed3c72fd8b1ff165a8652f64c0dde3bd021ab94b
sophialhou17 <sophia.lanxin.hou@gmail.com>
2025-01-15 15:44:39 -0500

    Shelf brown

--- client/src/components/modules/Shelf.jsx
+++ client/src/components/modules/Shelf.jsx
@@ -5,9 +5,8 @@ import "./Shelf.css";
 const Shelf = (props) => {
   return (
     <div className="Shelf-container">
-      <div className="Shelf-item">
-        <img src="../../assets/plants/testplant.jpg" alt="Plant" />
-      </div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
       <div className="Shelf-item">hi</div>
       <div className="Shelf-item">hi</div>
       <div className="Shelf-item">hi</div>
