56e6a77f44f8ba631311a4921934ada17dcd11fa
sophialhou17 <sophia.lanxin.hou@gmail.com>
2025-01-15 16:01:38 -0500

    plant in shelf

--- client/src/components/modules/Shelf.jsx
+++ client/src/components/modules/Shelf.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
+import testPlant from "../../assets/plants/testplant.jpeg";
 
 import "./Shelf.css";
 
 const Shelf = (props) => {
   return (
     <div className="Shelf-container">
-      <div className="Shelf-item">hi</div>
+      <img src={testPlant} alt="Plant image" className="Shelf-item"/>
       <div className="Shelf-item">hi</div>
       <div className="Shelf-item">hi</div>
       <div className="Shelf-item">hi</div>
