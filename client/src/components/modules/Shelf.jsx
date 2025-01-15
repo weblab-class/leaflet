375a44c1d4bbe7d6c072b923e9a7050b4e1b7de2
sophialhou17 <sophia.lanxin.hou@gmail.com>
2025-01-15 15:11:26 -0500

    Shelf basics

---
+++ client/src/components/modules/Shelf.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useContext } from "react";
+
+import "./Shelf.css";
+
+const Shelf = (props) => {
+    return (
+        <div className="Shelf-container">
+            <div>hi</div>
+            <div>hi</div>
+            <div>hi</div>
+            <div>hi</div>
+            <div>hi</div>
+            <div>hi</div>
+            <div>hi</div>
+            <div>hi</div>
+        </div>
+    )
+}
+
+export default Shelf;
