527292e04fef4422ecc399957a9803adcc2104a2
thegoodbagel <zhang.abrianna@gmail.com>
2025-01-16 17:14:55 -0500

    added bookReader

---
+++ client/src/components/modules/Book.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useContext } from "react";
+import "./Book.css";
+
+const Book = (props) => {
+  return (
+    <div className="Book-container">
+      <SinglePlant url="testplant.jpeg" />
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+      <div className="Shelf-item">hi</div>
+    </div>
+  );
+};
+
+export default Shelf;
--- client/src/components/pages/BookReader.jsx
+++ client/src/components/pages/BookReader.jsx
@@ -1,10 +1,30 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
+import { get, post } from "../../utilities";
 
-import "../../utilities.css";
-import { UserContext } from "../App";
+const BookReader = ({ title }) => {
+  const [book, setBook] = useState([]); // Array of pages (text)
+  const [curPage, setCurPage] = useState(-1); // Current page index
 
-const BookReader = () => {
-  return;
+  const getBook = () => {
+    const data = get(`/api/Book?title=${title}`);
+    setBook(data.content);
+    setCurPage(data.curPage);
+  };
+
+  const getPage = (pageNumber) => {
+    post(`/api/setPage`, title, pageNumber);
+    setCurPage(pageNumber);
+  };
+
+  useEffect(() => {
+    getBook();
+  }, []);
+
+  return (
+    <div>
+      <p>{book[curPage]}</p>
+    </div>
+  );
 };
 
 export default BookReader;
