de37f1d33b09fd2583c5d44bd61f0c397edf1f64
thegoodbagel <zhang.abrianna@gmail.com>
2025-01-28 19:54:17 -0500

    BookSuggest

--- client/src/components/modules/BookSuggest.jsx
+++ client/src/components/modules/BookSuggest.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Booksuggest.css";
+import "./BookSuggest.css";
 
 const fetchSuggestions = (searchTitle, setSuggestions, setLoading, signal) =>
   new Promise((resolve, reject) => {
